3db1cb0ad93f3055cb4ba453248f366d58a2350a
Ahlame <65197536+AhlameT@users.noreply.github.com>
2024-11-18 21:35:30 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def monghistogramme():
     return render_template("histogramme.html")
+
+@app.route("/commits/")
+def mescommits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
